c0c9d4a838aadfc0fdb4074d426092f998c31143
Duncan Murray <djmurray@gmail.com>
2017-07-28 22:37:54 +0930

    config for itnobam

---
+++ ITNOBAM/itnobam/config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# config.py 
+#
+# Definitions and ontology to map area that is to be logged
+
+areas = [
+    {'name':'Bedroom', 'sensor_id':'br01'},
+    {'name':'Lounge', 'sensor_id':'lounge'},
+    {'name':'Kitchen', 'sensor_id':'kitchen'},
+    {'name':'Hall', 'sensor_id':'hall'},
+    {'name':'Garage', 'sensor_id':'garage'},
+    {'name':'Front yard', 'sensor_id':'front'},
+    {'name':'Back Yard', 'sensor_id':'back'},
+    ]
+    
+people = [
+    {'name':'YourName', 'id':'me'},
+    {'name':'Frank', 'id':'primary_carer'},
+    {'name':'Jane', 'id':'secondary_carer'},
+    {'name':'Nick', 'id':'doctor'},
+    ]
+    
