25c59e6691932b4fae0f590006f887fef59e6a68
Duncan Murray <djmurray@gmail.com>
2017-07-31 00:18:53 +0930

    initial dummy commit of monitor.py to scan for locations of user in home

---
+++ ITNOBAM/itnobam/monitor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# monitor.py 
+#
+# module to handle the sensors and monitoring of users world
+
+def main():
+    """
+    main function used for testing 
+    """
+    s = Sensors()
+    s.scan()
+    return s.which_room_am_i_in()
+
+    
+    
+class Sensors:
+    def scan(self):
+        """
+        scan all sensors
+        """
+        import config
+        for area in config.areas:
+            #print('area = ',area)
+            #print('area = ',area[sensor_id])
+            #print('k,v = ', k,v)
+            for k,v in area.items():
+                if k == 'sensor_id':
+                    print('scanning ' + v)
+            
+        return 'TODO'
+
+    def which_room_am_i_in(self):
+        return 'Bedroom'
+
+    
+if __name__ == '__main__':
+    unittest.main()
