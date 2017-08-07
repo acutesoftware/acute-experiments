afdd5fbc9dcfc68d8f50e7346456397d8a12ec7c
Duncan Murray <djmurray@gmail.com>
2017-08-07 22:11:37 +0930

    using People class to manage users known contacts

--- ITNOBAM/itnobam/who.py
+++ ITNOBAM/itnobam/who.py
@@ -14,30 +14,48 @@ sys.path.append(pth)
 
 import core_data
 
-def main():
+def TEST():
+    p = People()
+    print(p)
+    
+    fam = p.get_family()
+    print(fam)
+
+class People:
     """
-    main function used for testing 
+    manages all the people user has contact with 
     """
-    everyone = core_data.CoreDataWho('Everyone')
-    family = core_data.CoreDataWho('Family',  parent=everyone)
-    carers = core_data.CoreDataWho('Carers',  parent=everyone)
-    friends = core_data.CoreDataWho('Friends',  parent=everyone)
+    def __init__(self):
+        everyone = core_data.CoreDataWho('Everyone')
+        family = core_data.CoreDataWho('Family',  parent=everyone)
+        carers = core_data.CoreDataWho('Carers',  parent=everyone)
+        friends = core_data.CoreDataWho('Friends',  parent=everyone)
 
 
-    people = []
-    people.append(core_data.CoreDataWho('Me',   ['Frank',  'Physical', 'Self'],    parent=everyone))
-    people.append(core_data.CoreDataWho('Wife', ['Maggie', 'Physical', 'Family'],  parent=family))
-    people.append(core_data.CoreDataWho('Son',  ['George', 'Physical', 'Family'],  parent=family))
-    people.append(core_data.CoreDataWho('Doctor',  ['Doctor Nick', 'Physical', 'Carer'],  parent=carers))
-    people.append(core_data.CoreDataWho('John',  ['John', 'Physical', 'Friend'],  parent=friends))
-    people.append(core_data.CoreDataWho('Jane',  ['Jane', 'Physical', 'Friend'],  parent=friends))
+        self.people = []
+        self.people.append(core_data.CoreDataWho('Me',   ['Frank',  'Physical', 'Self'],    parent=everyone))
+        self.people.append(core_data.CoreDataWho('Wife', ['Maggie', 'Physical', 'Family'],  parent=family))
+        self.people.append(core_data.CoreDataWho('Son',  ['George', 'Physical', 'Family'],  parent=family))
+        self.people.append(core_data.CoreDataWho('Doctor',  ['Doctor Nick', 'Physical', 'Carer'],  parent=carers))
+        self.people.append(core_data.CoreDataWho('John',  ['John', 'Physical', 'Friend'],  parent=friends))
+        self.people.append(core_data.CoreDataWho('Jane',  ['Jane', 'Physical', 'Friend'],  parent=friends))
     
-    
-    for p in people:
-        print(p.format_dict())
-    
+    def __str__(self):
+        res = ''
+        for p in self.people:
+            res += p.format_dict() + '\n'
+        
+        return res
 
-
+    
+    def get_family(self):
+        res = []
+        for p in self.people:
+            if p.data[2] == 'Family':
+                res.append(p.name)
+                
+        return res
+    
     
 if __name__ == '__main__':
-    main()
+    TEST()
