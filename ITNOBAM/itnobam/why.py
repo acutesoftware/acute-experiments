c3bb37623814cbca485be5365ca5f2e94db3bd63
Duncan Murray <djmurray@gmail.com>
2017-09-06 00:14:18 +0930

    working out initial scenario samples

--- ITNOBAM/itnobam/why.py
+++ ITNOBAM/itnobam/why.py
@@ -5,19 +5,27 @@
 import os
 import sys
 
-# Note - once finalised, use the following line
-#            import aikif.lib.cls_context as context
-# but for now add in the path and use local copy
-root_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..") ) 
-pth = os.path.join(root_folder, 'AIKIF', 'aikif', 'environments' )
-sys.path.append(pth)
-import room as env
-pth2 = os.path.join(root_folder, 'AIKIF', 'aikif', 'lib' )
-sys.path.append(pth2)
-import cls_context as context
+import who
+import what
+import where
 
+scenarios = [
+    'memory loss',
+    'lack of movement',
+    'mild confusion',
+    ]
+            
 
+preferred_outcome = [
+    'cognitive ability',
+    'physical freedom',
+    'financial independance',
+    ]
     
+    
+path_to_solve = []
+    
+            
 def main():
     """
     main function used for testing 
