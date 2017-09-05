bd4cefd0385f049410d77f4d090bfb750e9dd200
Duncan Murray <djmurray@gmail.com>
2017-09-06 00:13:31 +0930

    initial setup

--- ITNOBAM/itnobam/when.py
+++ ITNOBAM/itnobam/when.py
@@ -9,11 +9,8 @@ import sys
 #            import aikif.lib.cls_context as context
 # but for now add in the path and use local copy
 root_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..") ) 
-pth = os.path.join(root_folder, 'AIKIF', 'aikif', 'environments' )
+pth = os.path.join(root_folder, 'AIKIF', 'aikif', 'lib' )
 sys.path.append(pth)
-import room as env
-pth2 = os.path.join(root_folder, 'AIKIF', 'aikif', 'lib' )
-sys.path.append(pth2)
 import cls_context as context
 
 ontology_time = [
