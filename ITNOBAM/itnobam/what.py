ec8699d90c4b2a13029750fc0fe477ddaf4be410
Duncan Murray <djmurray@gmail.com>
2017-09-05 23:18:06 +0930

    removed unused import

--- ITNOBAM/itnobam/what.py
+++ ITNOBAM/itnobam/what.py
@@ -5,17 +5,6 @@
 import os
 import sys
 
-# Note - once finalised, use the following line
-#            import aikif.lib.cls_context as context
-# but for now add in the path and use local copy
-root_folder = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "..") ) 
-pth = os.path.join(root_folder, 'AIKIF', 'aikif', 'environments' )
-sys.path.append(pth)
-import room as env
-pth2 = os.path.join(root_folder, 'AIKIF', 'aikif', 'lib' )
-sys.path.append(pth2)
-import cls_context as context
-
 things = [
     'chair',
     'bed',
@@ -23,8 +12,6 @@ things = [
     'Computer',
     'Mobile',
     ]
-    
-
     
     
 def main():
