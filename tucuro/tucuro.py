dc8c50b7d258a96fd736dd6f1033db8e95824329
acutesoftware <djmurray@acutesoftware.com.au>
2019-02-17 01:11:12 +1030

    initial commit for overall interface

---
+++ tucuro/tucuro.py
@@ -0,0 +1,21 @@
+# tucuro.py
+
+def connect():
+  """
+  connects to a gaming platform, sending character data
+  """
+  pass
+
+def import_stats():
+  """
+  downloads stats from a gaming platform and imports to
+  local store
+  """
+  pass
+
+def send_character_stats():
+  """
+  sends your character statistics to the gaming platform
+  via a mapping function defined by the platform
+  """
+  pass
