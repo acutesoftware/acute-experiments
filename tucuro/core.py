768348a7b18b84a601901dfcac9d996732a36135
acutesoftware <djmurray@acutesoftware.com.au>
2019-02-17 01:11:44 +1030

    base mapping values for characters

---
+++ tucuro/core.py
@@ -0,0 +1,12 @@
+# core.py
+# This is the BASE mapping ontology and statistics
+# Other mappings for games use this and map to it
+# with modifiers to suit their game.
+# for example level_max is overrided with a hard value
+# but level speed is a multiplier
+
+level_speed = 1.000
+level_start = 1
+level_max = 100
+skill_speed = 1.000
+skill_start = 1
