efb091fc951de208301e9941473ac16b963b5236
acutesoftware <djmurray@acutesoftware.com.au>
2019-07-08 21:38:47 +0930

    open json file from interface if exists

--- tucuro/src/tucuro.py
+++ tucuro/src/tucuro.py
@@ -3,21 +3,29 @@
 # tucuro.py
 
 import os
-
+import json 
 
 def main():
     interface_list = [d for d in os.listdir('interface') if os.path.isdir(os.path.join('interface',d))]
     for interface in interface_list:
-        print('import stats from ' + interface)
+        import_game_data(interface)
 
 
-def import_stats(game):
+def import_game_data(interface):
     """
     imports stats to standard format using definitions
     in 'interface' folder
     """
+    try:
+        with open(os.path.join('interface', interface, 'map.json')) as f:
+            print('importing stats from ' + interface)
+            res = f.read()
+            read_game_stats(res)
+    except Exception as ex:
+        print('no mapping file defined for ' + interface)
 
-
+def read_game_stats(json_raw):
+    print(json_raw)
 
 
 if __name__ == "__main__":
