e75bc367a7decb68c469fe7c45b3e9b991b6277c
Duncan Murray <djmurray@gmail.com>
2022-09-08 23:42:26 +0930

    updated valid lists and moved old code to prototype

--- ITNOBAM/src/config/valid_lists.py
+++ ITNOBAM/src/config/valid_lists.py
@@ -2,7 +2,7 @@
 # valid lists for robot - central place to define available functions
 # 
 
-all_bots = [
+all_bots = [  # list of bots - keep both data and code in one module for now
     'def_robot',
     'def_console',
     'def_sensor',
@@ -10,7 +10,7 @@ all_bots = [
     'def_webapp',
 ]
 
-all_inputs = [
+all_inputs = [  # what can the bot see
     'camera', 
     'light',
     'sound', 
@@ -18,18 +18,44 @@ all_inputs = [
     'keyboard'
     ]
 
-all_outputs = [
+all_outputs = [  # how does the bot commnunicate with the outside world
     'voice', 
     'log', 
     'network', 
     'screen']
 
-all_networks = [
+all_networks = [  # trivial string to stop casual users starting bot on command line
     '54AE231B0802A1D409',
     '010203040506070809',
 ]
 
-trusted_humans = [
+trusted_humans = [  # which humans should the bot be taking commands from
     'duncan',
     'doctor_mary'
+]
+
+all_statuses = [  # what is the bot doing right now
+    'BOOTING',
+    'IDLE',
+    'LEARNING',
+    'PLAYING',
+    'CHATTING',
+    'WORKING',
+    'ALERT',
+    'DANGER'
+]
+
+all_moods = [   # what mood is the bot right now (initially random or based on environment)
+    'NORMAL',
+    'PLAYFUL',
+    'LAZY',
+    'NERVOUS'
+]
+
+all_goals = [   # what is the goal the bot is working towards
+    'ASSIST',
+    'GUARD',
+    'LEARN',
+    'CLEAN',
+    'PLAY'
 ]
