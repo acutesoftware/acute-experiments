246234b0dde1f2ac29e1f9336793d427082bdd06
acutesoftware <djmurray@gmail.com>
2024-01-25 23:06:12 +1030

    working on menu:

--- cyberdeck/src/menu.py
+++ cyberdeck/src/menu.py
@@ -10,11 +10,17 @@ def main():
     print('WELCOME TO CYBERDECK')
     current_menu = 'ROOT'
     conn = sqlite3.connect(utils.db_file)
-    menu_sql = 'SELECT parent_id, menu_id FROM p_menu'
+    menu_sql = 'SELECT parent_id, menu_id, sort_order,menu_text,help_text FROM p_menu'
     res = utils.get_data(conn, menu_sql, [])
-    for row_num, row in enumerate(res):
-        if row[0] == current_menu:
-            print(str(row_num) + ' = ' + row[1])
+
+    while current_menu != 'EXIT':
+        for row_num, row in enumerate(res):
+            if row[0] == current_menu:
+                print(row[1] + ' = ' + row[3])
+        x = input('Enter menu > ')
+        current_menu = x.upper()
+        #if current_menu
+
 
 
 if __name__ == '__main__':
